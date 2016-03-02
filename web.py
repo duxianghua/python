8e070fe2fdea75355ead05a16f7300701374a51a
duxianghua <duxianghua@duxianghuadeMacBook-Pro.local>
2016-03-02 14:34:15 +0800

    new

--- web.py
+++ web.py
@@ -3,7 +3,16 @@ app = Flask(__name__)
 
 @app.route('/index.html')
 def index():
-	return render_template('index.html')
+	a=['192.168.1.223','ok','13ms','21.23','7 day','12312','32','2 day','4.32.4','2016-03-01']
+	return render_template('index.html'navigation=a)
+
+@app.route('/test.html')
+def index():
+	return render_template('test.html',page='sdfsdfsdf')
+
+@app.route('/<pagename>')
+def page():
+	return pagename
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port='80')
